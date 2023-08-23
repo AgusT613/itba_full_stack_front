17cff92894c78e7d76a016d5f9ff8c233cfd08f2
AgusT613 <torresagustin613@gmail.com>
2023-08-23 11:37:29 -0300

    Estilos y responsive. Implementacion de mensajes para el usuario. Redireccionamiento a la pagina principal

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Contenedores
-const contenedorFormulario = document.querySelector('.contenedor__form')
+const contenedorFormulario = document.querySelector('.contenedor__formulario')
 const contenedorMensaje = document.querySelector('.contenedor__mensaje')
 // Link de Página Principal
 const paginaPrincipal = './pagina_principal/pagina_principal.html'
@@ -9,6 +9,17 @@ const usuario = {
     contraseña: 1234
 }
 
+const validaciones = {
+    campoVacio: 'Complete los campos',
+    datosIncorrectos: 'Datos de usuario incorrectos'
+}
+
+const crearMensaje = (contenedorPadre, mensaje)=>{
+    // Si el contenedor del mensaje tiene un elemento hijo no muestra el mensaje otra vez
+    if (contenedorPadre.hasChildNodes() && contenedorPadre.textContent === mensaje) return
+    contenedorPadre.textContent = mensaje
+}
+
 const loguearUsuario = (evento)=>{
     // Detiene el comportamiento por defecto del navegador
     evento.preventDefault()
@@ -16,18 +27,19 @@ const loguearUsuario = (evento)=>{
     const objetoFormulario = new FormData(contenedorFormulario)
     const datosUsuario = Object.fromEntries(objetoFormulario)
     let { nombreUsuario, contraseña} = datosUsuario
-    console.log(datosUsuario);
+
+    // Si los campos estan vacios
+    if (nombreUsuario === '' || contraseña === '') {
+        crearMensaje(contenedorMensaje, validaciones.campoVacio)
+    }
     // En caso de algun error en los datos muestra un mensaje de error al usuario
-    if (nombreUsuario !== usuario.nombreUsuario || parseInt(contraseña) !== usuario.contraseña){
-        // Si el contenedor del mensaje tiene un elemento hijo no muestra el mensaje otra vez
-        if (contenedorMensaje.hasChildNodes()) return
+    else if (nombreUsuario !== usuario.nombreUsuario || parseInt(contraseña) !== usuario.contraseña){
         // Crea un elemento HTML <p> e inserta un texto
-        const contenedorTexto = document.createElement('p')
-        const mensaje = 'Nombre de usuario y contraseña incorrecto'
-        contenedorTexto.textContent = mensaje
-        contenedorMensaje.appendChild(contenedorTexto)
-    } else {
-        // Sino redirige hacia la pagina principal
+        crearMensaje(contenedorMensaje, validaciones.datosIncorrectos)
+    }
+    // Sino redirige hacia la pagina principal
+    else {
+        contenedorFormulario.reset()
         location.href = paginaPrincipal
     }
 }
