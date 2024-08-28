83c7e7cc32aaffcae7438e94b96d3f00b2152a03
AgusT613 <torresagustin613@gmail.com>
2024-08-28 14:39:12 -0300

    feat(homePage): add bank resources section

---
+++ components/Welcome/BankResourceContainer.jsx
@@ -0,0 +1,19 @@
+import styles from "@/components/Welcome/Welcome.module.css"
+
+export default function BankResourceContainer ({resourceList}){
+    const { title, description, href, icon } = resourceList
+
+    return (
+        <div className={styles.bankResources__itemBox}>
+            <div>
+                {icon}
+                <h5 className={styles.bankResources__itemBox__title}>{title}</h5>
+            </div>
+            <p className={styles.bankResources__itemBox__description}>{description}</p>
+            <a href={href} className={styles.bankResources__itemBox__moreInfoLink}>
+                <span>Conocer Más</span>
+                <span>+</span>
+            </a>
+        </div>
+    )
+}
--- components/Welcome/Welcome.jsx
+++ components/Welcome/Welcome.jsx
@@ -1,6 +1,8 @@
 import HeaderHome from '../HeaderHome/HeaderHome'
 import styles from './Welcome.module.css'
 import AutoImagesCarrousel from './AutoImagesCarrousel'
+import bankResources from './bankResourcesList'
+import BankResourceContainer from './BankResourceContainer'
 
 export default function Welcome () {
   return (
@@ -8,19 +10,24 @@ export default function Welcome () {
       <div className={styles.wrapperContainer}>
         <HeaderHome />
         <main>
-          <div className={styles.heroSection}>
-            <section className={styles.heroSection__titles}>
+          <section className={styles.heroSection}>
+            <div className={styles.heroSection__titles}>
               <h1>Banco Online ITBANK</h1>
               <h2>Homebanking</h2>
               <span>Tu banco al alcance de tu celular</span>
               <p>Nos enorgullece brindarte una experiencia bancaria en línea que es conveniente, segura y fácil de usar. Nuestro portal de Homebanking está diseñado para ofrecerte un acceso completo y personalizado a tus cuentas financieras, permitiéndote administrar tus finanzas desde la comodidad de tu hogar o cualquier lugar donde te encuentres.</p>
-            </section>
+            </div>
             <figure className={styles.heroSection__carrousel}>
               <AutoImagesCarrousel />
             </figure>
-          </div>
-          <section>
-            More Content Soon
+          </section>
+          <section className={styles.bankResources}>
+            <h4 className={styles.bankResources__title}>¿Qué estas buscando?</h4>
+            <div className={styles.bankResources__items}>
+              {bankResources.map(resource => (
+                <BankResourceContainer resourceList={resource} key={resource[0]}/>
+              ))}
+            </div>
           </section>
         </main>
       </div>
