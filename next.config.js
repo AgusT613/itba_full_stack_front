942899389135ba46a852bb4289609b58f46407d6
AgusT613 <torresagustin613@gmail.com>
2024-11-12 16:47:57 -0300

    eslint: add prettier, airbnb and modify config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,29 @@
+const path = require("path")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'randomuser.me',
-        port: '',
-        pathname: '/api/**'
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "randomuser.me",
+        port: "",
+        pathname: "/api/**",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.plugins = config.plugins || []
+
+    config.optimization.providedExports = true
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
