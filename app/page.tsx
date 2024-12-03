800138f2975eafc7c954d666533cdece9d911911
Agus Torres <torresagustin613@gmail.com>
2024-12-03 10:11:10 -0300

    refactor(homePage): bankAction section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import BankActionContainer from '@/components/bankActionContainer/BankActionContainer'
-import bankActions from '@/components/bankActionContainer/bankActionContainer.module.css'
 import FooterHome from '@/components/footerHome/FooterHome'
 import HeaderHome from '@/components/HeaderHome/HeaderHome'
+import BankActionWrapper from '@/components/home/bankActions/bankActionWrapper'
 import BankResourceWrapper from '@/components/home/bankResources/BankResourceWrapper'
 import Hero from '@/components/home/hero/Hero'
-import { BANK_ACTIONS } from '@/utils/userDataModel'
 
 import { Wrapper } from './page.panda'
 
@@ -16,16 +14,7 @@ export default function Page() {
         <main>
           <Hero />
           <BankResourceWrapper />
-
-          {/* BANK ACTIONS */}
-          <section className={bankActions.actionContainer} id='actions'>
-            {BANK_ACTIONS.map((action) => (
-              <BankActionContainer
-                bankAction={action}
-                key={action.description}
-              />
-            ))}
-          </section>
+          <BankActionWrapper />
         </main>
       </Wrapper>
       <FooterHome />
---
+++ components/home/bankActions/bankActionWrapper.tsx
@@ -0,0 +1,14 @@
+import styles from '@/components/home/bankActions/bankActionWrapper.module.css'
+import { BANK_ACTIONS } from '@/utils/userDataModel'
+
+import BankActionContainer from './bankActionContainer/BankActionContainer'
+
+export default function BankActionWrapper() {
+  return (
+    <section className={styles.bankActions} id='actions'>
+      {BANK_ACTIONS.map((action) => (
+        <BankActionContainer bankAction={action} key={action.description} />
+      ))}
+    </section>
+  )
+}
