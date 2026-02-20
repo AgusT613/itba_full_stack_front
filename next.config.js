5decec6b4f84c25557449f741433b2941f386ade
Agus Torres <torresagustin613@gmail.com>
2026-02-20 16:00:03 -0300

    fix(config): eslint and nextjs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -11,21 +9,6 @@ const nextConfig = {
         pathname: '/api/**',
       },
     ],
-  },
-  webpack: (config) => {
-    config.plugins = config.plugins || []
-
-    config.optimization.providedExports = true
-
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './'),
-    }
-
-    return config
-  },
-  eslint: {
-    dirs: ['utils', 'hooks', 'context', 'app', 'components'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
 }
 
